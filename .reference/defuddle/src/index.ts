935df8d4539d4d0c253cef11ea6e2f0743cecddf
Gary Blankenship <gary@blankenship.me>
2026-05-03 20:43:13 -0400

    chore: remove .reference/ from git tracking
    .gitignore already listed .reference/ but files were tracked before
    the ignore rule was added. Untracked 72 reference files (defuddle +
    defuddle-cli upstream source). Files remain on disk locally.

--- .reference/defuddle/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { Defuddle } from './defuddle';
-export type { DefuddleOptions, DefuddleResponse, DefuddleMetadata } from './types';
-
-// Export Defuddle as default
-export default Defuddle; 
